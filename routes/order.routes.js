8bc0ddc90100b4f65ef93b118443bbf3156882b9
juanchixx <juanchixx@gmail.com>
2020-06-29 18:51:11 -0300

    New admin permissions for edit and del. orders

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -8,6 +8,6 @@ var TokenController = require('../controllers/token.controller')
 router.post('/order', TokenController.checkToken, OrderController.createOrder);
 router.get('/orders', TokenController.checkToken, TokenController.isAdmin, OrderController.getOrders);
 router.get('/order/:idOrder', OrderController.getOrder);
-router.put('/order/:idOrder', OrderController.updateOrder);
-router.delete('/order/:idOrder', OrderController.deleteOrder);
+router.put('/order/:idOrder', TokenController.checkToken, TokenController.isAdmin, OrderController.updateOrder);
+router.delete('/order/:idOrder', TokenController.checkToken, TokenController.isAdmin, OrderController.deleteOrder);
 module.exports = router;
